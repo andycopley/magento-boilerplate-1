193438173eb57fa056f82639367281c6fa2a5ed2
kallepersson <hi@kallepersson.se>
2013-02-07 17:23:51 +0100

    Moved templates from old installation

---
+++ app/code/community/AfonsoWilsson/AWCheckout/controllers/Checkout/CartController.php
@@ -0,0 +1,24 @@
+<?php
+include_once('Mage/Checkout/controllers/CartController.php');
+class AfonsoWilsson_AWCheckout_Checkout_CartController extends Mage_Checkout_CartController {
+
+    public function addAction() {
+        parent::addAction();
+        $this->_redirect('checkout/onepage');
+    }
+
+    public function updatePostAction() {
+        parent::updatePostAction();
+        $this->_redirect('checkout/onepage');
+    }
+
+    public function couponPostAction() {
+        parent::couponPostAction();
+        $this->_redirect('checkout/onepage');
+    }
+
+    public function deleteAction() {
+        parent::deleteAction();
+        $this->_redirect('checkout/onepage');
+    }
+}
