6057be9d0050750eeef25e8c6e6296cf5f22798e
kallepersson <hi@kallepersson.se>
2013-02-08 13:15:28 +0100

    Changed cart index action redirect

--- app/code/community/AfonsoWilsson/AWCheckout/controllers/Checkout/CartController.php
+++ app/code/community/AfonsoWilsson/AWCheckout/controllers/Checkout/CartController.php
@@ -2,11 +2,6 @@
 include_once('Mage/Checkout/controllers/CartController.php');
 class AfonsoWilsson_AWCheckout_Checkout_CartController extends Mage_Checkout_CartController {
 
-    public function indexAction() {
-        parent::indexAction();
-        $this->_redirect('checkout/onepage');
-    }
-
     public function addAction() {
         parent::addAction();
         $this->_redirect('checkout/onepage');
